bb1938a22ef672957efbfc7000274e6e53ff15e6
j-wye <4mimprove@gmail.com>
2025-04-20 00:10:15 +0900

    0420

--- scripts/train.py
+++ scripts/train.py
@@ -248,7 +248,7 @@ class IsaacEnvROS2(Node):
             self.collision_bool = False
         else:
             self.collision_bool = True
-        reward, self.done = self.reward_function(time_steps, max_episode_steps)
+        reward, self.done = self.reward_function_test_one(time_steps, max_episode_steps)
         return self.next_state, self.next_state_add, reward, self.done
             
     def reward_function(self, time_steps, max_episode_steps):
@@ -300,8 +300,8 @@ class IsaacEnvROS2(Node):
         
         progress_delta = self.remain_dist - self.prev_remain_dist
         progress_efficiency = kappa_p * progress_delta
-        if progress_delta < 0:
-            progress_efficiency *= 2
+        # if progress_delta < 0:
+        #     progress_efficiency *= 2
         
         reward = dist_efficiency + time_efficiency + progress_efficiency
         if reward <= -100 or time_steps >= max_episode_steps:
@@ -376,7 +376,7 @@ if __name__ == '__main__':
     parser.add_argument('--num_steps', type=int, default=12001, metavar='N', help='maximum number of steps (default: 5000)')
     parser.add_argument('--hidden_size', type=int, default=128, metavar='N', help='hidden size (default: 256)')
     parser.add_argument('--updates_per_step', type=int, default=1, metavar='N', help='model updates per simulator step (default: 1)')
-    parser.add_argument('--start_steps', type=int, default=5000, metavar='N',help='Steps sampling random actions (default: 10000)')
+    parser.add_argument('--start_steps', type=int, default=10000, metavar='N',help='Steps sampling random actions (default: 10000)')
     parser.add_argument('--target_update_interval', type=int, default=1, metavar='N', help='Value target update per no. of updates per step (default: 1)')
     parser.add_argument('--replay_size', type=int, default=500000, metavar='N', help='size of replay buffer (default: 10000000)')
     parser.add_argument('--automatic_entropy_tuning', type=bool, default=False, metavar='G', help='Automaically adjust α (default: False)')
